1478149221ddd2f4b5698d1a758786928e35be2d
Ali Aftab Muhammad <aliaftab9@hotmail.com>
2021-12-23 03:37:32 -0500

    update operation

--- backend/server.js
+++ backend/server.js
@@ -7,25 +7,58 @@ app.listen(3000, function() {
     console.log('listening on 3000')
   })
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
+  const MongoClient = require('mongodb').MongoClient
+  
+  MongoClient.connect("mongodb+srv://Renfrew100:d5oYc5sp0dBwgZZZ@cluster0.eyjik.mongodb.net/LaunchCode?retryWrites=true&w=majority",  {
+    useUnifiedTopology: true
+  })
+  .then(client => {
+    // ...
+    const db = client.db('star-wars-quotes')
+    const quotesCollection = db.collection('quotes')
 
+  })
 
+  app.post('/quotes', (req, res) => {
+  quotesCollection.insertOne(req.body)
+    .then(result => {
+      console.log(result)
+    })
+    .catch(error => console.error(error))
+})  
 
-// const MongoClient = require('mongodb').MongoClient
-
-/*   MongoClient.connect("mongodb+srv://Renfrew100:d5oYc5sp0dBwgZZZ@cluster0.eyjik.mongodb.net/LaunchCode?retryWrites=true&w=majority", {
+MongoClient.connect("mongodb+srv://Renfrew100:d5oYc5sp0dBwgZZZ@cluster0.eyjik.mongodb.net/LaunchCode?retryWrites=true&w=majority", {
     useUnifiedTopology: true
   }, (err, client) => {
     if (err) return console.error(err)
     console.log('Connected to Database')
+    const db = client.db('star-wars-quotes')
+    const quotesCollection = db.collection('quotes')
+    app.use(express.static('public'))
+    app.get(/* ... */)
+    app.post(/* ... */)
+    //app.post()
   })
- */
 
-app.post('/quotes', (req, res) => {
+/*  app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+}) */
+ 
+//app.set('view engine', 'ejs')
+
+// app.get('/', (req, res) => {
+//   db.collection('quotes').find().toArray()
+//     .then(/* ... */)
+//     .catch(/* ... */)
+//   res.render('index.ejs', {})
+// })
+
+// const MongoClient = require('mongodb').MongoClient
+ 
+
+/* app.post('/quotes', (req, res) => {
     console.log(req.body)
-})
+}) */
 
 /* app.post('/quotes', (req, res) => {
     console.log('Hellooooooooooooooooo!')
