a9054d737c39fd2a2ce3d3f2d425fec5bbb0c1d0
Ali Aftab Muhammad <aliaftab9@hotmail.com>
2021-12-24 20:32:15 -0500

    added axios for record

--- backend/routes/record.js
+++ backend/routes/record.js
@@ -38,37 +38,41 @@ recordRoutes.route("/record/:id").get(function (req, res) {
 
 // This section will help you create a new record.
 recordRoutes.route("/record/add").post(function (req, response) {
-  let db_connect = dbo.getDb();
-  let myobj = {
-    person_name: req.body.person_name,
-    person_position: req.body.person_position,
-    person_level: req.body.person_level,
-  };
-  db_connect.collection("records").insertOne(myobj, function (err, res) {
-    if (err) throw err;
-    response.json(res);
+    let db_connect = dbo.getDb();
+    let myobj = {
+      Rocket_name: req.body.Rocket_name,
+      Company_name: req.body.Company_name,
+      Successful: req.body.Successful,
+      Failed: req.body.Failed,
+      Postphoned: req.body.Postphoned
+    };
+    db_connect.collection("records").insertOne(myobj, function (err, res) {
+      if (err) throw err;
+      response.json(res);
+    });
   });
-});
 
 // This section will help you update a record by id.
 recordRoutes.route("/update/:id").post(function (req, response) {
-  let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId( req.params.id )};
-  let newvalues = {
-    $set: {
-      person_name: req.body.person_name,
-      person_position: req.body.person_position,
-      person_level: req.body.person_level,
-    },
-  };
-  db_connect
-    .collection("records")
-    .updateOne(myquery, newvalues, function (err, res) {
-      if (err) throw err;
-      console.log("1 document updated");
-      response.json(res);
-    });
-});
+    let db_connect = dbo.getDb();
+    let myquery = { _id: ObjectId( req.params.id )};
+    let newvalues = {
+      $set: {
+          Rocket_name: req.body.Rocket_name,
+          Company_name: req.body.Company_name,
+          Successful: req.body.Successful,
+          Failed: req.body.Failed,
+          Postphoned: req.body.Postphoned
+      },
+    };
+    db_connect
+      .collection("records")
+      .updateOne(myquery, newvalues, function (err, res) {
+        if (err) throw err;
+        console.log("1 document updated");
+        response.json(res);
+      });
+  });
 
 // This section will help you delete a record
 recordRoutes.route("/:id").delete((req, response) => {
