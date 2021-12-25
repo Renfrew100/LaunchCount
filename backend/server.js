6d0f9172e01ffc0600fe07fdfdb1cb44479d8509
Ali Aftab Muhammad <aliaftab9@hotmail.com>
2021-12-24 19:27:08 -0500

    another way to connect

--- backend/server.js
+++ backend/server.js
@@ -1,68 +1,19 @@
-console.log("May the node be with you")
-
-const express = require('express');
+const express = require("express");
 const app = express();
-
-app.listen(3000, function() {
-    console.log('listening on 3000')
-  })
-
-  const MongoClient = require('mongodb').MongoClient
-  
-  MongoClient.connect("mongodb+srv://Renfrew100:d5oYc5sp0dBwgZZZ@cluster0.eyjik.mongodb.net/LaunchCode?retryWrites=true&w=majority",  {
-    useUnifiedTopology: true
-  })
-  .then(client => {
-    // ...
-    const db = client.db('star-wars-quotes')
-    const quotesCollection = db.collection('quotes')
-
-  })
-
-  app.post('/quotes', (req, res) => {
-  quotesCollection.insertOne(req.body)
-    .then(result => {
-      console.log(result)
-    })
-    .catch(error => console.error(error))
-})  
-
-MongoClient.connect("mongodb+srv://Renfrew100:d5oYc5sp0dBwgZZZ@cluster0.eyjik.mongodb.net/LaunchCode?retryWrites=true&w=majority", {
-    useUnifiedTopology: true
-  }, (err, client) => {
-    if (err) return console.error(err)
-    console.log('Connected to Database')
-    const db = client.db('star-wars-quotes')
-    const quotesCollection = db.collection('quotes')
-    app.use(express.static('public'))
-    app.get(/* ... */)
-    app.post(/* ... */)
-    //app.post()
-  })
-
-/*  app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-}) */
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/record"));
+// get driver connection
+const dbo = require("./db/conn");
  
-//app.set('view engine', 'ejs')
-
-// app.get('/', (req, res) => {
-//   db.collection('quotes').find().toArray()
-//     .then(/* ... */)
-//     .catch(/* ... */)
-//   res.render('index.ejs', {})
-// })
-
-// const MongoClient = require('mongodb').MongoClient
+app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
  
-
-/* app.post('/quotes', (req, res) => {
-    console.log(req.body)
-}) */
-
-/* app.post('/quotes', (req, res) => {
-    console.log('Hellooooooooooooooooo!')
-})
-
-
- */
+  });
+  console.log(`Server is running on port: ${port}`);
+});
