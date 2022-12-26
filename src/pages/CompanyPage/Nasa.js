f21d0fe539d2e0d31ce893e9a22981bfa1d5a5bc
Ali Aftab Muhammad <aliaftab9@hotmail.com>
2022-12-26 15:37:52 -0500

    reversed heroku changes

--- src/pages/CompanyPage/Nasa.js
+++ src/pages/CompanyPage/Nasa.js
@@ -10,7 +10,7 @@ const Nasa = () => {
     
     const getRockets = async () => {
       try {
-        const rockets = await sendRequest("https://launch-count.herokuapp.com/rockets/Nasa")
+        const rockets = await sendRequest("https://launch-code.herokuapp.com/rockets/Nasa")
         setRocketDatabaseData(rockets)
       } catch (err) {}
     }
--- src/pages/CompanyPage/SpaceX.js
+++ src/pages/CompanyPage/SpaceX.js
@@ -10,7 +10,7 @@ const SpaceX = () => {
   useEffect(() => {
     const getRockets = async () => {
       try {
-        const rockets = await sendRequest("https://launch-count.herokuapp.com/rockets/SpaceX")
+        const rockets = await sendRequest("https://launch-code.herokuapp.com/rockets/SpaceX")
         setRocketDatabaseData(rockets)
       } catch (err) {}
     }
