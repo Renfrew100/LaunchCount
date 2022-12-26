038dfccbb44a3a667af48f0b238ff314e343c305
Ali Aftab Muhammad <aliaftab9@hotmail.com>
2022-12-26 14:42:35 -0500

    slight changes with heroku

--- src/pages/CompanyPage/Blue Origin.js
+++ src/pages/CompanyPage/Blue Origin.js
@@ -10,7 +10,7 @@ const BlueOrigin = () => {
   useEffect(() => {
     const getRockets = async () => {
       try {
-        const rockets = await sendRequest("https://launch-code.herokuapp.com/rockets/Blue Origin")
+        const rockets = await sendRequest("https://launch-count.herokuapp.com/rockets/Blue Origin")
         setRocketDatabaseData(rockets)
       } catch (err) {}
     }
